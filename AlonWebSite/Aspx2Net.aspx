4a059e9fcfe99f6915776ffd051877a595e4cd1a
Alon <User@302-08>
2025-12-02 16:58:42 +0200

    לוח הכפל

---
+++ AlonWebSite/Aspx2Net.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Aspx2Net.aspx.cs" Inherits="Aspx2Net" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+
+<%=strKefel %>
+
+    
+</body>
+</html>
---
+++ AlonWebSite/Aspx2Net.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Aspx2Net : System.Web.UI.Page
+{
+    public string strKefel;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        strKefel = "<table border= '1' >";
+        for (int i = 1; i <= 10; i++)
+        {
+            
+                strKefel += "<tr>";
+                for (int j = 1; j <= 10; j++)
+                {
+                    strKefel += "<td> ";
+                    strKefel += i * j;
+                    strKefel += "</td>";
+                }
+                strKefel += "</tr>";
+
+            
+            
+
+        }
+        strKefel += "</table>";
+    }
+}
